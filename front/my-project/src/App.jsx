cfffdcbd5ba00aa7880e678cb72e301ad0c3c248
Eduardo <42901658+LopezSennin@users.noreply.github.com>
2023-12-03 22:22:49 -0500

    Login terminado y avance de Navbar

--- front/my-project/src/App.jsx
+++ front/my-project/src/App.jsx
@@ -1,23 +1,36 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import './App.css'
-import NavBar from './components/NavBar'
+
+import { useContext} from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 //vistas 
 import HomeLogin from './views/homeLogin'
-
+import NavBar from './components/NavBar'
+import Home from './views/home'
 
 function App() {
 
+  const {isAuthenticated} = useAuth0();
 
+  
   return (
     <>
       
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/homelogin" />} />
-          <Route path="/homelogin" element={<HomeLogin />} />
-        </Routes>
-      </BrowserRouter>
+      
+        {isAuthenticated ? (
+          <BrowserRouter>
+            <NavBar/>
+            <Routes>
+              <Route path="/" element={<Navigate to="/home" />} />
+              <Route path="/home" element={<Home/>} />
+            </Routes>
+          </BrowserRouter>
+        ):
+        (
+          <HomeLogin />         
+        )}
+      
     </>
     
     
