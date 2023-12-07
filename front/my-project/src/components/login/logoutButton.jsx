0ddd22a92c1130a873f46cd1999ba0016f48a052
Eduardo <42901658+LopezSennin@users.noreply.github.com>
2023-12-06 23:06:56 -0500

    mejora en NavBar
    se elimino la campana de notificaciones, se organizo la acción del botón App, eliminar campana de notificaciones y alargar botón de LogOut

--- front/my-project/src/components/login/logoutButton.jsx
+++ front/my-project/src/components/login/logoutButton.jsx
@@ -5,7 +5,7 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button className="btn btn-square btn-ghost" onClick={() => logout({ returnTo: window.location.origin } )}>
+    <button className="btn btn-ghost" onClick={() => logout({ returnTo: window.location.origin } )}>
       Log Out
     </button>
   );
