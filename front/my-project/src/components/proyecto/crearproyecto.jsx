365de5997d9cee769e2759f0b81b2c53e9cdcb0d
joan <joan.calderon@correounivalle.edu.co>
2023-12-10 18:09:54 -0500

    ok crearproyecto

--- front/my-project/src/components/proyecto/crearproyecto.jsx
+++ front/my-project/src/components/proyecto/crearproyecto.jsx
@@ -5,25 +5,25 @@ function Crearproyecto() {
     <div>
       <h1 className='font-sans font-medium text-4xl pb-14 pt-6 text-center'>Crear Proyecto</h1>      
         <div className="flex justify-center">
-          <form >
+          <form className="w-96">
             
             <div>
               <label className='block font-sans font-medium text-xl'>Nombre</label>
-              <input type="text" className="input input-bordered w-full max-w-xs my-2" />
+              <input type="text" className="input input-bordered w-full my-2" />
             </div>
 
             <div>
               <label className='block font-sans font-medium text-xl'>Fecha inicio</label>
-              <input type="date" className="input input-bordered w-full max-w-xs my-2" />
+              <input type="date" className="input input-bordered w-full my-2" />
             </div>
 
             <div>
               <label className='block font-sans font-medium text-xl'>Fecha limite</label>
-              <input type="date" className="input input-bordered w-full max-w-xs my-2" />
+              <input type="date" className="input input-bordered w-full my-2" />
             </div>
 
             <div>
-              <button className="btn ">Agregar Proyecto</button>
+              <button className="btn btn-outline w-full">Agregar Proyecto</button>
             </div>
    
           </form>
